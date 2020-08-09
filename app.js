fb0c81825cf136f34dd6f70d4de558ae3b997c13
FireFrog <monjurul.47@gmail.com>
2020-08-09 13:25:56 +0600

    V2

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const duration = document.querySelector('#duration');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
 const resetBtn = document.querySelector('#reset');
+const resumeBtn = document.querySelector('#resume');
+const circle = document.querySelector('circle');
 const ticking = new Audio('ticking.mp3');
 const timeup = new Audio('timeup.mp3');
-const circle = document.querySelector('circle');
+
 
 const perimeter = circle.getAttribute('r') * Math.PI * 2;
 circle.setAttribute('stroke-dasharray', perimeter);
@@ -12,34 +14,67 @@ circle.setAttribute('stroke-dasharray', perimeter);
 let reduceOffset, currentOffset = 0;
 
 class Timer {
-    constructor(duration, startBtn, pauseBtn) {
+    constructor(duration, startBtn, pauseBtn, resetBtn, resumeBtn) {
         this.duration = duration;
         this.startBtn = startBtn;
         this.pauseBtn = pauseBtn;
         this.resetBtn = resetBtn;
+        this.resumeBtn = resumeBtn;
 
 
         this.startBtn.addEventListener('click', this.start);
         this.pauseBtn.addEventListener('click', this.pause);
         this.resetBtn.addEventListener('click', this.reset);
+        this.resumeBtn.addEventListener('click', this.resume);
+
+        resumeBtn.disabled = true;
+        pauseBtn.disabled = true;
     }
 
     start = () => {
-        startBtn.disabled = true;
         reduceOffset = (perimeter / duration.value);
         currentOffset -= reduceOffset;
         this.tick();
         this.interval = setInterval(this.tick, 1000);
+
+        startBtn.disabled = true;
+        pauseBtn.disabled = false;
     };
 
     pause = () => {
         clearInterval(this.interval);
-        startBtn.disabled = false;
+
+        startBtn.disabled = true;
+        resumeBtn.disabled = false;
+        pauseBtn.disabled = true;
     };
+
+    userInput = () => {
+        this.pause();
+
+        resumeBtn.disabled = true;
+        startBtn.disabled = false;
+        pauseBtn.disabled = true;
+
+        circle.setAttribute('stroke-dashoffset', 0);
+        currentOffset = 0;
+    }
+
+    resume = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+
+        resumeBtn.disabled = true;
+        pauseBtn.disabled = false;
+    }
 
     reset = () => {
         this.pause();
         this.duration.value = 30;
+
+        resumeBtn.disabled = true;
+        startBtn.disabled = false;
+        pauseBtn.disabled = true;
 
         circle.setAttribute('stroke-dashoffset', 0);
         currentOffset = 0;
@@ -54,6 +89,8 @@ class Timer {
             circle.setAttribute('stroke-dashoffset', 0)
             currentOffset = 0;
 
+            resumeBtn.disabled = true;
+
         } else {
             ticking.play();
             this.duration.value = timeRamain - 1;
@@ -64,4 +101,4 @@ class Timer {
     };
 
 }
-const t1 = new Timer(duration, startBtn, pauseBtn, resetBtn);
+const t1 = new Timer(duration, startBtn, pauseBtn, resetBtn, resumeBtn);
